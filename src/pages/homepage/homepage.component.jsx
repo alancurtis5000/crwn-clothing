d09e6f5378768c84e14b60b634cfe224b39ee8e0
Alan <alancurtis5000+accounts@gmail.com>
2020-12-31 14:51:38 -0700

    structured pages and compontents basic styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import "./homepage.styles.scss";
+import Direcotry from "../../components/directory/directory.component";
+
+const Homepage = () => (
+  <div classname="homepage">
+    <Direcotry />
+  </div>
+);
+
+export default Homepage;
