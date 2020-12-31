93ae27992be7a5f457c4eec755944b3ec308bcc8
Alan <alancurtis5000+accounts@gmail.com>
2020-12-31 15:48:47 -0700

    added routing and navigation to directory

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Homepage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
