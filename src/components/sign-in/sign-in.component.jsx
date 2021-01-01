b56e0994b957c048ced877241608b4fc06a7744c
Alan <alancurtis5000+accounts@gmail.com>
2021-01-01 14:07:21 -0700

    made custome button to reuse

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import "./sign-in.styles.scss";
 import { Component } from "react";
 
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 class SignIn extends Component {
   constructor(props) {
@@ -46,7 +47,7 @@ class SignIn extends Component {
             label="Password"
             required
           />
-          <input type="submit" value="Submit Form" />
+          <CustomButton type="submit">Sign in</CustomButton>
         </form>
       </div>
     );
