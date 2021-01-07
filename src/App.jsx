a414934d3b5a71a6268ca22f790079f641c90617
Alan <alancurtis5000+accounts@gmail.com>
2021-01-06 21:50:25 -0700

    checkout page route/ empty message cart dd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Switch, Route, Redirect } from "react-router-dom";
+
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+
 import Header from "./components/header/header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { connect } from "react-redux";
@@ -42,6 +45,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/signIn"
---
+++ src/pages/checkout/checkout.component.jsx
@@ -0,0 +1,5 @@
+import "./checkout.styles.scss";
+
+const CheckoutPage = () => <div>Checkout pages</div>;
+
+export default CheckoutPage;
