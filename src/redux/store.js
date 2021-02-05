639c0966125a36f1c68c52084b2f2262cd6987ee
Alan <alancurtis5000+accounts@gmail.com>
2021-02-05 15:31:19 -0700

    added redux-persist for localstorage managment

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
-
+// redux-persist is used for persiting data on localstorage , or session storage
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
