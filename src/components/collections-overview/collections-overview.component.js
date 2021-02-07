8929b76517e2ec7e9796ae36205e1e1c7c0826ff
Alan <alancurtis5000+accounts@gmail.com>
2021-02-07 11:55:46 -0700

    added new selector for preview

--- src/components/collections-overview/collections-overview.component.js
+++ src/components/collections-overview/collections-overview.component.js
@@ -1,7 +1,7 @@
 import "./collections-overview.styles.scss";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { selectCollections } from "../../redux/shop/shop.selectors";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
 const CollectionOverview = ({ collections }) => (
@@ -13,6 +13,6 @@ const CollectionOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 export default connect(mapStateToProps)(CollectionOverview);
