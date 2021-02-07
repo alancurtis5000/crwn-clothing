e46c4822962572459d27b9ae94bdae59501ed618
Alan <alancurtis5000+accounts@gmail.com>
2021-02-07 11:45:05 -0700

    add: styles/ and data normalization/ optimization

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -5,10 +5,16 @@ import { selectCollection } from "../../redux/shop/shop.selectors";
 import CollectionItem from "../../components/collection-item/collection-item.component";
 
 const CollectionPage = ({ match, collection }) => {
+  const { title, items } = collection;
   console.log(collection);
   return (
     <div className="collection-page">
-      <h2>collection PAGE</h2>
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
       {match.params.collectionId}
     </div>
   );
