ac40ee74603ccca73be642a9e30e9f68883acaf9
Alan <alancurtis5000+accounts@gmail.com>
2021-02-09 00:34:10 -0700

    getting collections data from firebase

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,49 @@
+import React from "react";
 import { Route } from "react-router-dom";
 import CollectionOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
+import { connect } from "react-redux";
+import { updateCollections } from "../../redux/shop/shop.actions";
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
 
-const ShopPage = ({ match }) => {
-  return (
-    <div className="shop-page">
-      <Route exact path={`${match.path}`} component={CollectionOverview} />
-      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-    </div>
-  );
-};
+class ShopPage extends React.Component {
+  unsubscribeFromSnapshot = null;
+  componentDidMount() {
+    const { updateCollections } = this.props;
+    const collectionRef = firestore.collection("collections");
+    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
+      async (snapshot) => {
+        const collectionMap = convertCollectionsSnapshotToMap(snapshot);
+        console.log(collectionMap);
+        updateCollections(collectionMap);
+      }
+    );
+  }
+  componentWillUnmount() {
+    // need to unsubscribe
+  }
 
-export default ShopPage;
+  render() {
+    const { match } = this.props;
+
+    return (
+      <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  updateCollections: (collectionsMap) =>
+    dispatch(updateCollections(collectionsMap)),
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
