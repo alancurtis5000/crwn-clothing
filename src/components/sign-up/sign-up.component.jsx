289ab2b5517ac1c3abbbb04ab258c2456510392f
Alan <alancurtis5000+accounts@gmail.com>
2021-03-04 23:31:30 -0700

    207: user sign up to sagas

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,8 +1,8 @@
 import { Component } from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-
-import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
+import { signUpStart } from "../../redux/user/user.actions";
+import { connect } from "react-redux";
 
 import "./sign-up.styles.scss";
 
@@ -18,6 +18,7 @@ class SignUp extends Component {
   }
 
   handleSubmit = async (event) => {
+    const { signUpStart } = this.props;
     event.preventDefault();
     const { displayName, email, password, confirmPassword } = this.state;
     if (password !== confirmPassword) {
@@ -25,21 +26,12 @@ class SignUp extends Component {
       return;
     }
 
-    try {
-      const { user } = await auth.createUserWithEmailAndPassword(
-        email,
-        password
-      );
-      await createUserProfileDocument(user, { displayName });
-      this.setState({
-        displayName: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-      });
-    } catch (error) {
-      console.log(error.message);
-    }
+    const userCredentials = {
+      displayName,
+      email,
+      password,
+    };
+    signUpStart(userCredentials);
   };
 
   handleChange = (event) => {
@@ -93,4 +85,8 @@ class SignUp extends Component {
   }
 }
 
-export default SignUp;
+const mapDispatchToProps = (dispatch) => ({
+  signUpStart: (userCredentials) => dispatch(signUpStart(userCredentials)),
+});
+
+export default connect(null, mapDispatchToProps)(SignUp);
